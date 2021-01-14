e666afc6fc96f0164c4f0759b954b34dd701269f
Brennan Grieve <46986566+brennanGrieve@users.noreply.github.com>
2021-01-14 17:51:25 +1300

    Getspecs implemented + tidied up repeated code

--- api/get/getRecent.php
+++ api/get/getRecent.php
@@ -1,18 +1,11 @@
 <?php
     require(__DIR__.'/getConn.php');
 
-    $data = $conn->query("SELECT * FROM Stock ORDER BY DateAdded LIMIT 12");
+    $statement = $conn->query("SELECT * FROM Stock ORDER BY DateAdded LIMIT 12");
 
-    $rows = $data->fetchAll();
+    $data = $statement->fetchAll();
 
-    $json;
-    $i = 0;
-    foreach($rows as $row){
-        $json[$i] = $row;
-        $i++;
-    }
-
-    $response = json_encode($json);
+    require(__DIR__.'/parseOutput.php');
 
     echo $response;
     ?>
