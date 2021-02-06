4f7296a88f2127e3302b1e74cabc6ed329eb44c0
Brennan Grieve <46986566+brennanGrieve@users.noreply.github.com>
2021-02-06 21:20:51 +1300

    Config file updated + New GET user established on RDBMS

---
+++ api/get/getUserAccessConn.php
@@ -0,0 +1,6 @@
+<?php
+    require(__DIR__.'/../../config/config.php');
+    $target = "mysql:host=localhost;dbname=".$userDB.";";
+
+    $conn = new PDO($target, $getUDataConnUser, $getUDataConnPass);
+    ?>
---
+++ api/get/getUserNameUniqueness.php
@@ -0,0 +1,4 @@
+<?php
+    require(__DIR__.'/getUserAccessConn.php');
+    $searchTerms = $_GET["name"];
+?>
