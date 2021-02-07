efedaacc73c3ff89d118e46e3d321dafdcd1b6ae
Brennan Grieve <46986566+brennanGrieve@users.noreply.github.com>
2021-02-07 19:21:12 +1300

    Username check script initial implementation
    The output should be parsed by the frontend to determine whether a username can be used.

--- api/get/getUserNameUniqueness.php
+++ api/get/getUserNameUniqueness.php
@@ -1,4 +1,10 @@
 <?php
     require(__DIR__.'/getUserAccessConn.php');
     $searchTerms = $_GET["name"];
+
+    $statement = $conn->prepare("SELECT redacted FROM redacted WHERE user = ?");
+    $statement->execute(array($searchTerms));
+    $data = $statement->fetchAll();
+    require(__DIR__.'/parseOutput.php');
+    $response = json_encode($json);
 ?>
