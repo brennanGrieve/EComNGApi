c9f2e4e0a0103e6cd1f4128e0eecbf00ebeaa463
Brennan Grieve <46986566+brennanGrieve@users.noreply.github.com>
2021-02-07 19:58:47 +1300

    Uniqueness check implemented and confirmed working

--- api/get/getUserNameUniqueness.php
+++ api/get/getUserNameUniqueness.php
@@ -1,10 +1,9 @@
 <?php
     require(__DIR__.'/getUserAccessConn.php');
     $searchTerms = $_GET["name"];
-
-    $statement = $conn->prepare("SELECT redacted FROM redacted WHERE user = ?");
+    $statement = $conn->prepare("SELECT redacted FROM redacted WHERE redacted = ?");
     $statement->execute(array($searchTerms));
     $data = $statement->fetchAll();
     require(__DIR__.'/parseOutput.php');
-    $response = json_encode($json);
+    echo $response;
 ?>
