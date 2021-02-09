24d183141e274e550a6d36f2a4338bb28a1ad2b6
Brennan Grieve <46986566+brennanGrieve@users.noreply.github.com>
2021-02-09 21:31:28 +1300

    Implemented an appropriate hashing algorithm + Salt for passwords

--- api/post/postNewAcc.php
+++ api/post/postNewAcc.php
@@ -1,6 +1,5 @@
 <?php
     require(__DIR__.'/postConn.php');
-
     $obj = json_decode(file_get_contents('php://input'));
     $newUserName = $obj->user;
     $newPass = $obj->pass;
@@ -9,7 +8,10 @@
     $newFName = $obj->fName;
     $newLName = $obj->lName;
     $newPhNum = $obj->phNum;
-    
-    $statement = $conn->prepare('INSERT INTO Redacted VALUES(?, ?, ?, ?)');
-    $statement->execute(array($newUserName, $newPass, $newEmail, $newShippingAddress));
+    $salt = random_bytes(512);
+    $saltedPass = $newPass.$salt;
+    $hashedPass = hash("sha512", $newPass.$salt);
+
+    $statement = $conn->prepare('INSERT INTO redacted VALUES(?, ?, ?, ?, ?, ? ,?, ?)');
+    $statement->execute(array($newUserName, $hashedPass, $newEmail, $newFName, $newLName, $newShipAddr, $newPhNum, $salt));
 ?>
