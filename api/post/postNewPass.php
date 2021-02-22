7517c0b1309bf69fdb90d95acc5d552635a2ad35
Brennan Grieve <46986566+brennanGrieve@users.noreply.github.com>
2021-02-22 18:32:43 +1300

    PostNewPass confirmed to work properly

--- api/post/postNewPass.php
+++ api/post/postNewPass.php
@@ -1,11 +1,14 @@
 <?php
     require(__DIR__.'/postConn.php');
     $obj = json_decode(file_get_contents('php://input'));
+    $passToCheck = $obj->oldPass;
+    $getSalt = $conn->prepare("SELECT salt FROM UserAuthData WHERE authToken = ?");
+    $getSalt->execute(array($_COOKIE["auth"]));
+    $saltResult =  $getSalt->fetch(PDO::FETCH_OBJ);
+    echo $saltResult;
     $salt = random_bytes(512);
     $pass = $obj->pass1;
     $hashedPass = hash("SHA512", $pass.$salt);
     $statement = $conn->prepare("UPDATE UserAuthData SET PASSHASH = ? WHERE authToken = ?");
-    $conn->execute(array($hashedPass, $_COOKIE["auth"]));
-    require(__DIR__.'/../auth/refreshAuthToken.php');
-    echo $authJSON;
+    $statement->execute(array($hashedPass, $_COOKIE["auth"]));
 ?>
