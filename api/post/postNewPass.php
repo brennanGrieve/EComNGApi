f20ba55277488a3c91fd97da63f073109a454c4e
Brennan Grieve <46986566+brennanGrieve@users.noreply.github.com>
2021-02-22 18:47:14 +1300

    Final Cleanup

--- api/post/postNewPass.php
+++ api/post/postNewPass.php
@@ -1,14 +1,16 @@
 <?php
     require(__DIR__.'/postConn.php');
     $obj = json_decode(file_get_contents('php://input'));
-    $passToCheck = $obj->oldPass;
-    $getSalt = $conn->prepare("SELECT salt FROM UserAuthData WHERE authToken = ?");
+    $passToCheck = $obj[1];
+    $getSalt = $conn->prepare("SELECT salt, USER_NAME FROM UserAuthData WHERE authToken = ?");
     $getSalt->execute(array($_COOKIE["auth"]));
     $saltResult =  $getSalt->fetch(PDO::FETCH_OBJ);
-    echo $saltResult;
-    $salt = random_bytes(512);
-    $pass = $obj->pass1;
+    $salt = $saltResult->salt;
+    $user = $saltResult->USER_NAME;
+    $pass = $obj[0];
     $hashedPass = hash("SHA512", $pass.$salt);
     $statement = $conn->prepare("UPDATE UserAuthData SET PASSHASH = ? WHERE authToken = ?");
     $statement->execute(array($hashedPass, $_COOKIE["auth"]));
+    require(__DIR__.'/../auth/refreshAuthToken.php');
+    echo $authJSON;
 ?>
