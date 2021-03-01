c7a505bfc0a928767a11091939f4af37dcc9e12b
Brennan Grieve <46986566+brennanGrieve@users.noreply.github.com>
2021-03-01 18:15:50 +1300

    Added proper checking for the correct existing password
    Need to handle this change on the frontend before testing further

--- api/post/postNewPass.php
+++ api/post/postNewPass.php
@@ -2,15 +2,18 @@
     require(__DIR__.'/postConn.php');
     $obj = json_decode(file_get_contents('php://input'));
     $passToCheck = $obj[1];
-    $getSalt = $conn->prepare("SELECT salt, USER_NAME FROM UserAuthData WHERE authToken = ?");
+    $getSalt = $conn->prepare("SELECT salt, PASSHASH FROM UserAuthData WHERE authToken = ?");
     $getSalt->execute(array($_COOKIE["auth"]));
     $saltResult =  $getSalt->fetch(PDO::FETCH_OBJ);
-    $salt = $saltResult->salt;
-    $user = $saltResult->USER_NAME;
     $pass = $obj[0];
-    $hashedPass = hash("SHA512", $pass.$salt);
-    $statement = $conn->prepare("UPDATE UserAuthData SET PASSHASH = ? WHERE authToken = ?");
-    $statement->execute(array($hashedPass, $_COOKIE["auth"]));
-    require(__DIR__.'/../auth/refreshAuthToken.php');
-    echo $authJSON;
+    $hashToCheck = hash("SHA512", $passToCheck.$saltResult->salt);
+    if($hashToCheck == $saltResult->PASSHASH){
+        $hashedPass = hash("SHA512", $pass.$saltResult->salt);
+        $statement = $conn->prepare("UPDATE UserAuthData SET PASSHASH = ? WHERE authToken = ?");
+        $statement->execute(array($hashedPass, $_COOKIE["auth"]));
+        require(__DIR__.'/../auth/refreshAuthToken.php');
+        echo $authJSON;
+    }else{
+        echo json_encode(array('failed' => '1'));
+    }
 ?>
