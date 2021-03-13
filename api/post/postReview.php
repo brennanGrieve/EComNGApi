981fc0f6da7cd60bfcaa653bceef7d17f09015ec
Brennan Grieve <46986566+brennanGrieve@users.noreply.github.com>
2021-03-13 22:23:54 +1300

    In the process of testing

--- api/post/postReview.php
+++ api/post/postReview.php
@@ -1,3 +1,20 @@
 <?php
+
+    require(__DIR__.'/postConn.php');
     
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
+    
+    $getUser = $conn->prepare('SELECT USER_NAME FROM UAuthData WHERE authToken = ?');
+    $getUser->execute(array($_COOKIE["auth"]));
+    $userName = $getUser->fetch(PDO::FETCH_OBJ);
+    var_dump($userName);
+
+    require(__DIR__.'/postReviewConn.php');
+    $obj = json_decode(file_get_contents('php://input'));
+
+    $statement = $conn->prepare('INSERT INTO ItemReviews VALUES(?,?,?,?, curdate())');
+    //$statement->execute(array($userName->USER_NAME, $obj[0], $obj[1], $obj[2]));
 ?>
