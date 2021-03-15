ce125c836e6f7b1eb0e361c71d1a7b7ecd24c838
Brennan Grieve <46986566+brennanGrieve@users.noreply.github.com>
2021-03-15 23:14:59 +1300

    Fixing syntax errors that prevented the script from completing properly

--- api/get/getUserReview.php
+++ api/get/getUserReview.php
@@ -1,15 +1,17 @@
 <?php
     require(__DIR__.'/getUserAccessConn.php');
-    $getUser = $conn->prepare('SELECT USER_NAME FROM UserAuthData WHERE auth = ?');
-    $getUser->execute(array($_COOKIE['auth']));
-    $name = $getUser->fetchAll(PDO::FETCH_OBJ);
+
+    $getUser = $conn->prepare('SELECT USER_NAME FROM UserAuthData WHERE authToken = ?');
+    $getUser->execute(array($_COOKIE["auth"]));
+    $userName = $getUser->fetch(PDO::FETCH_OBJ);
 
 
     require(__DIR__.'/getReviewConn.php');
 
-    $statement = $conn->prepare('SELECT score, comment FROM ItemReviews WHERE USER_NAME = ?');
-    $statement->execute(array($name["USER_NAME"]));
-    $result = $statement->fetchall(PDO::OBJ);
+    $statement = $conn->prepare('SELECT score, comment FROM ItemReviews WHERE USER_NAME = ? && id = ?');
+    $statement->execute(array($userName->USER_NAME, $_GET["id"]));
+    $result = $statement->fetchall(PDO::FETCH_OBJ);
+
 
     echo json_encode($result);
 
