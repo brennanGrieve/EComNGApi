2c8536d7f990577af861db06c3eb849552ce82b3
Brennan Grieve <46986566+brennanGrieve@users.noreply.github.com>
2021-03-15 21:12:08 +1300

    Update getUserReview.php

--- api/get/getUserReview.php
+++ api/get/getUserReview.php
@@ -1,3 +1,16 @@
 <?php
+    require(__DIR__.'/getUserAccessConn.php');
+    $getUser = $conn->prepare('SELECT USER_NAME FROM UserAuthData WHERE auth = ?');
+    $getUser->execute(array($_COOKIE['auth']));
+    $name = $getUser->fetchAll(PDO::FETCH_OBJ);
+
+
+    require(__DIR__.'/getReviewConn.php');
+
+    $statement = $conn->prepare('SELECT score, comment FROM ItemReviews WHERE USER_NAME = ?');
+    $statement->execute(array($name["USER_NAME"]));
+    $result = $statement->fetchall(PDO::OBJ);
+
+    echo json_encode($result);
 
 ?>
