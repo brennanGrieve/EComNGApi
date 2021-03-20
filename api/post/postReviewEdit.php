e04214f4297482b8586750c3456ce4a3e0a8a973
Brennan Grieve <46986566+brennanGrieve@users.noreply.github.com>
2021-03-20 21:04:06 +1300

    Fixing syntax error that prevented script execution

--- api/post/postReviewEdit.php
+++ api/post/postReviewEdit.php
@@ -7,5 +7,5 @@
      require(__DIR__.'/postReviewConn.php');
      $obj = json_decode(file_get_contents('php://input'));
      $statement = $conn->prepare("UPDATE ItemReviews SET score = ?, comment = ? WHERE id = ? && USER_NAME = ?");
-     $statement->execute(array($obj[1], $obj[2], $obj[0], $username->USER_NAME));
+     $statement->execute(array($obj[1], $obj[2], $obj[0], $userName->USER_NAME));
 ?>
