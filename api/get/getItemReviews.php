5dca5b0db43bafb025087ec29d61f3ea3cae52eb
Brennan Grieve <46986566+brennanGrieve@users.noreply.github.com>
2021-03-27 12:05:35 +1300

    getItemReviews.php intial implementation

---
+++ api/get/getItemReviews.php
@@ -0,0 +1,11 @@
+<?php
+    require(__DIR__.'/getReviewConn.php');
+
+    $statement = $conn->prepare("SELECT * FROM Reviews WHERE id = ? LIMIT 5 [offset ?]");
+    $statement->execute(array($_GET["id"], $_GET["offset"]));
+
+    $data = $statement->fetchAll();
+    require(__DIR__.'/parseOutput.php');
+    echo $response;
+    
+?>
