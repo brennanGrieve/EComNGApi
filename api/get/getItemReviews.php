8f92748a8f7dfe383faf54c38a5f58165bd11971
Brennan Grieve <46986566+brennanGrieve@users.noreply.github.com>
2021-03-27 15:30:29 +1300

    Laying groundwork for empty response handling + Fixing up issues
    Parameters have to be bound in a way that's explicitly typed or PDO throws a syntax error.

--- api/get/getItemReviews.php
+++ api/get/getItemReviews.php
@@ -1,11 +1,14 @@
 <?php
     require(__DIR__.'/getReviewConn.php');
 
-    $statement = $conn->prepare("SELECT * FROM ItemReviews WHERE id = ? LIMIT 5 [offset ?]");
-    $statement->execute(array($_GET["id"], $_GET["offset"]));
-
-    $data = $statement->fetchAll();
+    $statement = $conn->prepare("SELECT * FROM ItemReviews WHERE id = :id LIMIT 5 OFFSET :offset");
+    $statement->bindParam(':id', $_GET["id"], PDO::PARAM_INT);
+    $statement->bindParam(':offset', $_GET["offset"], PDO::PARAM_INT);
+    $statement->execute();
+    $data = $statement->fetchAll(PDO::FETCH_OBJ);
     require(__DIR__.'/parseOutput.php');
     echo $response;
+    
+
     
 ?>
